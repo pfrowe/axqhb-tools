e93ba9d31891eec02074f33b252f5111e2c3af62
Paul Rowe <paul@roweboat.com>
2026-02-26 17:46:41 -0600

    Implemented code-splitting.

--- resources/js/IndexRouter.tsx
+++ resources/js/IndexRouter.tsx
@@ -1,24 +1,42 @@
+import React, { Suspense, useCallback } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useAuth } from "./contexts/AuthContext";
-import { HomePage, LeaderboardPage, RalliesPage, RallyPage, SingersPage, TrampPage, UserPage } from "./pages";
 
 const IndexRouter = () => {
   const { user } = useAuth();
-  return user
-    ? (<Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/leaderboard/*" element={<LeaderboardPage />} />
-      <Route path="/rallies/*" element={<RalliesPage />} />
-      <Route path="/rally/*" element={<RallyPage />} />
-      <Route path="/singers/*" element={<SingersPage />} />
-      <Route path="/card/:unique_id" element={<TrampPage />} />
-      <Route path="/user/*" element={<UserPage />} />
-    </Routes>)
-    : (<Routes>
-      <Route path="/" element={<Navigate to="/user/login" />} />
-      <Route path="/card/:unique_id" element={<TrampPage />} />
-      <Route path="/user/*" element={<UserPage />} />
-    </Routes>);
+  const Loading = useCallback(() => <div>Loading...</div>, []);
+  let result;
+  if (user === undefined) {
+    const TrampPage = React.lazy(() => import("./pages/TrampPage"));
+    const UserPage = React.lazy(() => import("./pages/UserPage"));
+    result = (<Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<Navigate to="/user/login" />} />
+        <Route path="/card/:unique_id" element={<TrampPage />} />
+        <Route path="/user/*" element={<UserPage />} />
+      </Routes>
+    </Suspense>);
+  } else {
+    const HomePage = React.lazy(() => import("./pages/HomePage"));
+    const LeaderboardPage = React.lazy(() => import("./pages/LeaderboardPage"));
+    const RalliesPage = React.lazy(() => import("./pages/RalliesPage"));
+    const RallyPage = React.lazy(() => import("./pages/RallyPage"));
+    const SingersPage = React.lazy(() => import("./pages/SingersPage"));
+    const TrampPage = React.lazy(() => import("./pages/TrampPage"));
+    const UserPage = React.lazy(() => import("./pages/UserPage"));
+    result = (<Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/leaderboard/*" element={<LeaderboardPage />} />
+        <Route path="/rallies/*" element={<RalliesPage />} />
+        <Route path="/rally/*" element={<RallyPage />} />
+        <Route path="/singers/*" element={<SingersPage />} />
+        <Route path="/card/:unique_id" element={<TrampPage />} />
+        <Route path="/user/*" element={<UserPage />} />
+      </Routes>
+    </Suspense>);
+  }
+  return result;
 };
 IndexRouter.displayName = "IndexRouter";
 
